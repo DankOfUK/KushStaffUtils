c79afda3315de02bb7a217aa7ccb1fee876d853a
DankOfUK <sage123sage.123@gmail.com>
2024-02-13 23:25:46 +0000

    **added** location and blocks placed check

--- src/main/java/me/dankofuk/fkore/FKoreLeavePrinterLogger.java
+++ src/main/java/me/dankofuk/fkore/FKoreLeavePrinterLogger.java
@@ -1,13 +1,16 @@
 package me.dankofuk.fkore;
 
-import com.golfing8.kore.event.PlayerPrinterEnterEvent;
 import com.golfing8.kore.event.PlayerPrinterExitEvent;
 import com.golfing8.kore.feature.PrinterFeature;
+import com.golfing8.kore.object.PlayerPrinterInfo;
+import com.golfing8.kore.util.MaterialInfo;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import me.dankofuk.KushStaffUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +22,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
@@ -37,17 +41,22 @@ public class FKoreLeavePrinterLogger implements Listener {
         Player player = event.getPlayer();
         PlayerPrinterExitEvent.ExitReason reason = event.getExitReason();
         BigDecimal moneySpent = event.getPrinterInfo().getMoneySpent();
+        int xLocation = event.getPlayer().getLocation().getBlockX();
+        int yLocation = event.getPlayer().getLocation().getBlockY();
+        int zLocation = event.getPlayer().getLocation().getBlockZ();
+        String worldName  = Objects.requireNonNull(event.getPlayer().getLocation().getWorld()).getName();
+        Map<MaterialInfo, PlayerPrinterInfo.BlockRecord> blocksPlaced = event.getPrinterInfo().getPrintedBlocks();
         if (printerFeature.isInPrinter(player)) {
             KushStaffUtils.getInstance().getLogger().info(player.getName() + " exited Printer mode with reason: " + reason.toString());
             try {
-                sendWebhookToDiscord(player.getName(), reason.toString(), moneySpent);
+                sendWebhookToDiscord(player.getName(), reason.toString(), moneySpent, blocksPlaced, xLocation, yLocation, zLocation, worldName);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private void sendWebhookToDiscord(String playerName, String exitReason, BigDecimal moneySpent) {
+    private void sendWebhookToDiscord(String playerName, String exitReason, BigDecimal moneySpent, Map<MaterialInfo, PlayerPrinterInfo.BlockRecord> blocksPlaced, int xLocation, int yLocation, int zLocation, String worldName) {
         KushStaffUtils.getInstance().getLogger().info("Sending exit webhook to Discord");
         CompletableFuture.runAsync(() -> {
             try {
@@ -60,7 +69,10 @@ public class FKoreLeavePrinterLogger implements Listener {
                 JsonObject json = new JsonObject();
                 json.addProperty("username", "Printer Logger");
                 JsonObject embed = new JsonObject();
-                embed.addProperty("description", playerName + " has exited printer mode reason: " + exitReason + " and has spend " + moneySpent);
+                embed.addProperty("description", playerName + " has exited printer mode!\n Reason: "
+                        + exitReason + "\n Money Spent: " + moneySpent + "\n Blocks placed: "
+                        + blocksPlaced.toString() + "\n Location: " + "X:" + xLocation + " Y:" + yLocation
+                        + " Z:" + zLocation + " World: " + worldName);
                 embed.addProperty("title", "Printer Logger");
                 JsonArray embeds = new JsonArray();
                 embeds.add(embed);
